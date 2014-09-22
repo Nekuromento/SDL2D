26b0e5bc97e3aecc6c3b25e229c2bf69a068be3b
Nekuromento <max.klyga@gmail.com>
2014-09-22 16:25:48 +0300

    Add move constructor, operator =, size and swap methods

--- Engine/Util/FixedArray.hpp
+++ Engine/Util/FixedArray.hpp
@@ -1,13 +1,19 @@
 #ifndef FixedArray_h__
 #define FixedArray_h__
-#include "noncopyable.hpp"
+
+#include <algorithm>
+#include <cstdlib>
+#include <cassert>
+
+#include "Util/defines.hpp"
+#include "Util/noncopyable.hpp"
 
 namespace util {
 
 template <typename T>
 class FixedArray : public util::Noncopyable {
-    T* const ptr;
-    size_t size;
+    T* _ptr;
+    size_t _size;
 
     static const size_t elementSize = sizeof(T);
     static const size_t alignment = std::alignment_of<T>::value;
@@ -19,58 +25,85 @@ class FixedArray : public util::Noncopyable {
     }
 
 public:
+    FixedArray() :
+        _ptr{ nullptr },
+        _size{ 0 }
+    {}
+
     template <typename Allocator>
     FixedArray(Allocator& alloc, const size_t size) :
-        ptr {static_cast<T*>(alloc.allocate(elementSize * size, alignment, 0))},
-        size {size}
+        _ptr{ static_cast<T*>(alloc.allocate(elementSize * size, alignment, 0)) },
+        _size{ size }
     {
         std::uninitialized_fill(begin(), end(), T());
     }
 
     template <typename Allocator>
     FixedArray(Allocator& alloc, const FixedArray& other) :
-        ptr {static_cast<T*>(alloc.allocate(elementSize * other.size, alignment, 0))},
-        size {other.size},
+        _ptr{ static_cast<T*>(alloc.allocate(elementSize * other.size(), alignment, 0)) },
+        _size{ other.size() }
     {
         std::uninitialized_copy(other.begin(), other.end(), begin());
+    }
+
+    FixedArray(FixedArray&& other) :
+        _ptr{ other._ptr },
+        _size{ other._size }
+    {
+        other._ptr = nullptr;
+        other._size = 0;
     }
 
     ~FixedArray() {
         destroy(begin());
     }
 
+    FixedArray& operator =(FixedArray&& other) NOEXCEPT {
+        other.swap(*this);
+
+        return *this;
+    }
+
+    void swap(FixedArray& other) {
+        std::swap(_ptr, other._ptr);
+        std::swap(_size, other._size);
+    }
+
     REALLY_INLINE T& operator[] (const size_t index) NOEXCEPT {
-        assert(("Invalid index", index < size));
-        return ptr[index];
+        assert(("Invalid index", index < size()));
+        return _ptr[index];
     }
 
     REALLY_INLINE const T& operator[] (const size_t index) const NOEXCEPT {
-        assert(("Invalid index", index < size));
-        return ptr[index];
+        assert(("Invalid index", index < size()));
+        return _ptr[index];
     }
 
     REALLY_INLINE T* begin() NOEXCEPT {
-        return ptr;
+        return _ptr;
     }
 
     REALLY_INLINE const T* begin() const NOEXCEPT {
-        return ptr;
+        return _ptr;
     }
 
     REALLY_INLINE T* end() NOEXCEPT {
-        return ptr + size;
+        return _ptr + size();
     }
 
     REALLY_INLINE const T* end() const NOEXCEPT {
-        return ptr + size;
+        return _ptr + size();
     }
 
-    REALLY_INLINE size_t empty() const NOEXCEPT {
-        return size == 0;
+    REALLY_INLINE bool empty() const NOEXCEPT {
+        return size() == 0;
+    }
+
+    REALLY_INLINE size_t size() const NOEXCEPT {
+        return _size;
     }
 };
 
 }
-
 
 #endif // FixedArray_h__
